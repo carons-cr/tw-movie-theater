5d11e0c243efa29d0625947e58e684d7f613b398
ruochong <3056990458@qq.com>
2017-08-04 11:19:16 +0800

    withBoFang

--- Public/src/js/moviecontain.js
+++ Public/src/js/moviecontain.js
@@ -10,3 +10,15 @@ axios.get('/getMovie/'+myurl[1]).then(function (ans) {
     $('.gyf-detail').html(`  `+ans.data[0].detail);
     $('.gyf-origin_title').html(`原著名称：`+ans.data[0].origin_title);
 });
+$(document).ready(function () {
+    $(".btn").on('click', function () {
+        let str = '<embed src="https://imgcache.qq.com/tencentvideo_v1/playerv3/TPout.swf?max_age=86400&v=20161117&vid=e0024d8adt2&auto=0" allowFullScreen="true" quality="high" width="480" height="400" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>';
+        bootbox.confirm({
+            title: "Destroy planet?",
+            message: str,
+            callback: function (result) {
+                console.log('This was logged in the callback: ' + result);
+            }
+        });
+    });
+});
